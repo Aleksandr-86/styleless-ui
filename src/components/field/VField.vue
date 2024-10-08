9015d6a3bc4060831c7ec07479c3d9201989231f
Александр Карпенко <96790009+Aleksandr-86@users.noreply.github.com>
2024-10-08 13:38:20 +0300

    добавлены свойства prefix и suffix в компонент VField

--- src/components/field/VField.vue
+++ src/components/field/VField.vue
@@ -1,38 +1,46 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import VInput from '@/components/input/VInput.vue'
 
-interface Props {
-  // modelValue: string
-  /** Идентификатор поля ввода */
-  id: string | undefined
+import type { InputProps, InputEmits } from '@/components/input/VInput.vue'
+
+export interface FieldProps extends InputProps {
+  /** Текст перед полем ввода */
+  prefix?: string
+  /** Текст после поля ввода */
+  suffix?: string
 }
 
-defineProps<Props>()
+const { suffix } = defineProps<FieldProps>()
 
-const model = defineModel()
+interface FieldEmits extends InputEmits {}
 
-const greetingMessage = ref('0234{{ greetingMessaglakjfd }}29348')
+const emit = defineEmits<FieldEmits>()
+
+const model = defineModel({ type: String })
+
+const hasError = computed(() => true)
 </script>
 
 <template>
-  <!-- Контейнер компонента -->
   <div class="v-field__container">
-    <div class="v-field__slot_before"><slot name="before"></slot></div>
-
-    <!-- Контейнер поля ввода -->
-    <div class="v-field__input-container">
-      <div class="v-field__slot_prepend"><slot name="prepend"></slot></div>
-
-      <!-- Поле ввода -->
-      <VInput :id v-model="model" />
-
-      <div class="v-field__slot_append">
-        <slot :count="5" :text="greetingMessage" name="append"></slot>
-      </div>
+    <div v-if="!!$slots.prepend" class="v-field__slot_prepend">
+      <slot :has-error="hasError" name="prepend"></slot>
     </div>
 
-    <div class="v-field__slot_after"><slot name="after"></slot></div>
+    <div v-if="prefix" class="v-field__prefix">
+      <p>{{ prefix }}</p>
+    </div>
+
+    <VInput :id v-model="model" class="v-field__input" name="color" type="sum" />
+
+    <div v-if="suffix" class="v-field__suffix">
+      <p>{{ suffix }}</p>
+    </div>
+
+    <div v-if="!!$slots.append" class="v-field__slot_append">
+      <slot :has-error="hasError" name="append"></slot>
+    </div>
   </div>
 </template>
 
@@ -41,59 +49,27 @@ const greetingMessage = ref('0234{{ greetingMessaglakjfd }}29348')
   &__container {
     display: flex;
     align-items: center;
-    width: 100%;
-    overflow: hidden;
-  }
-
-  &__input-container {
-    display: flex;
-    align-items: center;
+    background-color: blueviolet;
     width: 100%;
     height: 100%;
-
-    // background-color: blueviolet;
+    overflow: hidden;
   }
 
   &__input {
     outline: none;
     background-color: transparent;
-    background-color: red;
+    background-color: wheat;
     width: 100%;
     height: 100%;
-
-    // background-color: wheat;
   }
 
-  &__slot_before,
   &__slot_prepend,
-  &__slot_append,
-  &__slot_after {
+  &__prefix,
+  &__suffix,
+  &__slot_append {
     display: flex;
     align-items: center;
     height: 100%;
   }
-
-  &__slot_before:empty,
-  &__slot_prepend:empty,
-  &__slot_append:empty,
-  &__slot_after:empty {
-    display: none;
-  }
-
-  // &__slot_before {
-  //   background-color: aqua;
-  // }
-
-  // &__slot_prepend {
-  //   background-color: aquamarine;
-  // }
-
-  // &__slot_append {
-  //   background-color: red;
-  // }
-
-  // &__slot_after {
-  //   background-color: yellowgreen;
-  // }
 }
 </style>
--- src/components/input/VInput.vue
+++ src/components/input/VInput.vue
@@ -1,27 +1,68 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { h } from 'vue'
 
-interface Props {
+export interface InputProps {
+  /** Область текста вместо поля ввода */
+  area?: boolean
+  /** Отключённое состояние поля ввода */
+  disabled?: boolean
   /** Идентификатор поля ввода */
-  id: string | undefined
+  id?: string
+  /** Имя поля ввода */
+  name?: string
+  /** Строка отображаемая в случае отсутствия какого-либо значения в поле ввода */
+  placeholder?: string
+  /** Состояние "только для чтения" */
+  readonly?: boolean
+  /** Тип поля ввода */
+  type?: 'email' | 'hidden' | 'number' | 'password' | 'sum' | 'search' | 'tel' | 'text'
 }
 
-defineProps<Props>()
+interface Props extends InputProps {
+  /** Модель */
+  modelValue?: string
+}
 
-const model = defineModel()
+const {
+  modelValue,
+  area,
+  disabled,
+  id,
+  name,
+  placeholder,
+  readonly,
+  type = 'text',
+} = defineProps<Props>()
+
+export interface InputEmits {
+  (e: 'update:modelValue', value: string): void
+}
+
+const emit = defineEmits<InputEmits>()
+
+function onInput({ target }: { target: EventTarget | null }) {
+  if (target !== null) {
+    const { value } = target as HTMLInputElement | HTMLTextAreaElement
+    emit('update:modelValue', value)
+  }
+}
+
+function Render() {
+  const typeAttr = type === 'sum' || type === 'tel' ? undefined : type
+
+  return h(area ? 'textarea' : 'input', {
+    value: modelValue,
+    onInput,
+    disabled,
+    id,
+    name,
+    placeholder,
+    readonly,
+    type: typeAttr,
+  })
+}
 </script>
 
 <template>
-  <input :id v-model="model" class="v-input" />
+  <Render />
 </template>
-
-<style lang="scss">
-.v-input {
-  outline: none;
-  background-color: transparent;
-  width: 100%;
-  height: 100%;
-
-  // background-color: wheat;
-}
-</style>
